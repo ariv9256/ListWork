30c22b76795999199e4cdac1055bdf7ee0acdafd
Angela Rivas <snowcats001@gmail.com>
2018-11-14 12:02:24 -0700

    Constructed all variables in model and started inputting JOptionPane and scanner constructors.

--- src/choice/controller/ChoiceController.java
+++ src/choice/controller/ChoiceController.java
@@ -1,6 +1,23 @@
 package choice.controller;
 
+import java.swing.JOptionPane;
+import java.util.Scanner;
+import java.util.ArrayList;
+
 public class ChoiceController
 {
-
+	private Scanner inputScanner;
+	private MovieModel movie;
+	
+	public ChoiceController()
+	{
+		movie = new MovieModel();
+		inputScanner = new Scanner(System.in);
+	}
+	public void start()
+	{
+		
+		
+	}
 }
+
--- src/choice/model/MovieModel.java
+++ src/choice/model/MovieModel.java
@@ -2,5 +2,53 @@ package choice.model;
 
 public class MovieModel
 {
-
+	private String movieName;
+	private int movieAge;
+	private int movieLength;
+	private boolean likeOrNot;
+	
+	
+	public MovieModel()
+	{
+		
+	}
+	public MovieModel(String movieName, int movieAge, int movieLength, boolean likeOrNot)
+	{
+		this.movieName = movieName;
+		this.movieAge = movieAge;
+		this.movieLength = movieLength;
+		this.likeOrNot = likeOrNot;
+	}
+	public String getMovieName()
+	{
+		return movieName;
+	}
+	public int getMovieAge()
+	{
+		return movieAge;
+	}
+	public int getMovieLength()
+	{
+		return movieLength;
+	}
+	public boolean getLikeOrNot()
+	{
+		return likeOrNot;
+	}
+	public void setMovieName(String movieName)
+	{
+		this.movieName = movieName;
+	}
+	public void setMovieAge(int movieAge)
+	{
+		this.movieAge = movieAge;
+	}
+	public void setMovieLength(int movieLength)
+	{
+		this.movieLength = movieLength;
+	}
+	public void setLikeOrNot(boolean likeOrNot)
+	{
+		this.likeOrNot = likeOrNot;
+	}
 }
